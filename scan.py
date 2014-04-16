1c8dc5df090c82e01c88498210dd42344638d3e5
AlfaroM <Alfaro.3@wright.edu>
2014-04-16 12:04:05 -0400

    Working on building obtaining local IP

--- scan.py
+++ scan.py
@@ -13,23 +13,50 @@ def main():
               "aptitude> sudo apt-get install python-nmap")
         exit(0)
     print("[*] Loading optparse libs")
+
+    help = """
+        Welcome to Eatsa_pizza's scanning tool!
+        Usage: ./scan.py [options] [host]
+        NOTE! -l option requires avahi-daemon
+    """
+
     from optparse import OptionParser
     parser = OptionParser()
+
+    #TODO: set IP to local address
     parser.add_option("-l", "--local-net", dest="localnet",
                       action="store_true", default="False",
                       help="Use this option to scan the local network")
 
+    #TODO: Look up interface specification
     parser.add_option("-i", "--interface", dest="iface", action="store", type="string",
                       help="Specify the interface to use i.e. wlan0, eth0...")
 
+    #TODO: get port range
     parser.add_option("-p", "--port", dest="port_range", action="store", type="string",
                       help="Specify the port or port range to scan")
 
+    #TODO: get IP range
     parser.add_option("-t", dest="ip_range", action="store", type="string",
                       help="IP range to scan. Valid formats are 1.1.1.1, 1.1.1.0-255, and 1.1.1.*")
 
+
+    #Fragmentation
     parser.add_option("-f", "--firewall", dest="firewall", action="store_true",
+                      default="true",
                       help="checks for possible existence of firewall")
+
+    #adding argv and argc from  parser.parse_args()
+    (options, args) = parser.parse_args()
+    if len(args) == 0:
+        print help
+        exit(0b1)
+
+    if options.localnet:
+        import scn_driver as scn
+        scn.scanner.getLoIP()
+
 
 if __name__ == "__main__":
     main()
+
